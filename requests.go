ef54c6c5bebe7afd8234df5a56edd89eac75a98d
nxshock <nxshock@gmail.com>
2019-01-15 20:30:44 +0500

    Fix login request structure

--- requests.go
+++ requests.go
@@ -1,13 +1,17 @@
 package matrixapi
 
-// https://matrix.org/docs/spec/client_server/r0.4.0.html#id213
+// LoginRequest represents login request
+// https://matrix.org/docs/spec/client_server/r0.4.0.html#post-matrix-client-r0-login
 type LoginRequest struct {
-	Type                     AuthenticationType `json:"type"`
-	User                     string             `json:"user"`
-	Password                 string             `json:"password"`
-	Token                    string             `json:"token,omitempty"`
-	DeviceID                 string             `json:"device_id,omitempty"`
-	InitialDeviceDisplayName string             `json:"initial_device_display_name,omitempty"`
+	Type                     AuthenticationType `json:"type"`                        // Required. The login type being used. One of: ["m.login.password", "m.login.token"]
+	Identifier               string             `json:"identifier"`                  // Identification information for the user.
+	User                     string             `json:"user"`                        // The fully qualified user ID or just local part of the user ID, to log in. Deprecated in favour of identifier.
+	Medium                   string             `json:"medium"`                      // When logging in using a third party identifier, the medium of the identifier. Must be 'email'. Deprecated in favour of identifier.
+	Address                  string             `json:"address"`                     // Third party identifier for the user. Deprecated in favour of identifier.
+	Password                 string             `json:"password"`                    // Required when type is m.login.password. The user's password.
+	Token                    string             `json:"token"`                       // Required when type is m.login.token. Part of Token-based login.
+	DeviceID                 string             `json:"device_id"`                   // ID of the client device. If this does not correspond to a known client device, a new device will be created. The server will auto-generate a device_id if this is not specified.
+	InitialDeviceDisplayName string             `json:"initial_device_display_name"` // A display name to assign to the newly-created device. Ignored if device_id corresponds to a known device.
 }
 
 // https://matrix.org/docs/spec/client_server/r0.4.0.html#id264
